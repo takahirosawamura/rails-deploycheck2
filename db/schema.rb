4004fb0187b17ee8e22d0b9087855026415f19c5
澤村隆寛 <cawst222@gmail.com>
2017-12-08 15:29:33 +0900

    modelなど

--- db/schema.rb
+++ db/schema.rb
@@ -9,14 +9,22 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+require 'active_record'
+require 'mysql'
 
-ActiveRecord::Schema.define(version: 20171206081255) do
+ActiveRecord::Schema.define(version: 20171207091338) do
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
