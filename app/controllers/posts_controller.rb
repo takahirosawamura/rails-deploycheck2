1fadf229c89b9fc865b47b648a001e98a9fe557d
澤村隆寛 <cawst222@gmail.com>
2017-12-08 16:15:23 +0900

    詳細ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    # find_byを用いてpostsテーブルから「params[:id]」に対応するデータを取り出し、変数@postに代入してください
+    @post = Post.find_by(id: params[:id])
+  end
 end
