bf27fc541750bb2da319cd58db3945b1ebe3dfba
澤村隆寛 <cawst222@gmail.com>
2017-12-12 21:48:03 +0900

    migrationファイルadduseridtopostsの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
+  # user_idカラムにバリデーションを追加してください
+  validates :user_id, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210105135) do
+ActiveRecord::Schema.define(version: 20171212122955) do
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171210105135) do
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
