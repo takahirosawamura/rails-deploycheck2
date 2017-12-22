630ab1a4f1a4b93aa8d4cded83deb81e84b95744
澤村隆寛 <cawst222@gmail.com>
2017-12-22 22:09:11 +0900

    ログアウト用のルーティングとアクションの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,4 +66,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "login" => "users#login_form"
 
   post "login" => "users#login"#oginの送信先
+  post "logout" => "users#logout"
 
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
