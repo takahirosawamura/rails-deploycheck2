a085230f0352f73e305f4f0e268c31f4a9bf1561
澤村隆寛 <cawst222@gmail.com>
2017-12-22 22:48:26 +0900

    アクション側で共通の変数を定義しよう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before_actionにset_current_userメソッドを指定
+  before_action: set_current_user
+
+
+  # set_current_userメソッドを定義してください
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
