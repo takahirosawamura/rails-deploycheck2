b792cca75e33b00329e707063089efa651de05ef
澤村隆寛 <cawst222@gmail.com>
2017-12-23 19:10:48 +0900

    いいねボタンを作ろう

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,19 @@
 class LikesController < ApplicationController
-  before_action:authenticate_user
+  before_action :authenticate_user
 
   def create
+    # 変数@likeを定義
+    @like = Like.new(
+      user_id: @current_user.id,
+      post_id: params[:post_id]
+      )
+
+
+    # 変数@likeを保存
+    @like.save
+
+
+    # 投稿詳細ページにリダイレクト
+    redirect_to("/posts/#{params[:post_id]}")
   end
 end
