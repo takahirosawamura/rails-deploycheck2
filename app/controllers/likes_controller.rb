1863f997de950473b76fb8b751adb2edd00cd0d4
澤村隆寛 <cawst222@gmail.com>
2017-12-23 19:19:34 +0900

    ™いいね取り消しボタン™

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,18 +2,18 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    # 変数@likeを定義
-    @like = Like.new(
-      user_id: @current_user.id,
-      post_id: params[:post_id]
-      )
-
-
-    # 変数@likeを保存
+    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
-
-
-    # 投稿詳細ページにリダイレクト
     redirect_to("/posts/#{params[:post_id]}")
   end
+
+  # destroyアクションを定義
+  def destroy
+    @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
+    @like.destroy
+    redirect_to("/posts/#{params[:post_id]}")
+  end
+
+
+
 end
