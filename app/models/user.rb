95afab4c2e35f2bcf17a19d7aba4ef55150f365e
澤村隆寛 <cawst222@gmail.com>
2017-12-24 22:01:34 +0900

    asset precompile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # nameカラムに関するバリデーションを作成
   validates :name, {presence: true}
-
-  # emailカラムに関するバリデーションを作成
   validates :email, {presence: true, uniqueness: true}
 
-  
 
   def posts
     return Post.where(user_id: self.id)
